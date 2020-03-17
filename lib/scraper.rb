5c4582128c06290268cac8ae551fc61f6e7c1fd3
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 20:24:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    #https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
+    page = Nokogiri::HTML(open(index_url))
+    students = []
+    page.css("div.student-card").each do |student| 
+      name = student.css(".student-name").text
+      location =
   end
 
   def self.scrape_profile_page(profile_url)
