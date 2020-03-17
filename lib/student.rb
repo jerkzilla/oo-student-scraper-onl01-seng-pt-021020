08188b8ed52ca93b691d20b0f13c8b8fa2a71730
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 20:39:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,10 +15,11 @@ class Student
 end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |k,v| self.send(("#{k}="), v)}
+    end
   end
 
   def self.all
-
+    @@all
   end
 end
