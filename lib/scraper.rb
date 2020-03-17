e8c978c1f817ac87e232b1f1c040973d1b490f09
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 20:28:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,10 @@ class Scraper
       student_info = {:name => name,
                       :location => location,
                       :profile_url => profile_url }
+                      students << student_info
   end
+  students 
+end
 
   def self.scrape_profile_page(profile_url)
 
